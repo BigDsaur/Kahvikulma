b8806f01fcc1e92ef9d369920e89a37f131aaadd
BigDsaur <Joona879@gmail.com>
2025-11-28 09:43:42 +0200

    css päivitetty

--- Kahvi_frontend/src/components/Navbar.jsx
+++ Kahvi_frontend/src/components/Navbar.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <ul className="navbar-links">
-        <li><a href="#meista">Tietoa meistä</a></li>
-        <li><a href="#yhteys">Yhteystiedot</a></li>
-        <li><a href="#aukiolo">Aukioloajat</a></li>
-      </ul>
+      <div className="nav-center">
+        <button 
+          className="hamburger" 
+          onClick={() => setOpen(!open)}
+        >
+          ☰
+        </button>
+
+        <ul className={`navbar-links ${open ? "open" : ""}`}>
+          <li><a href="#meista">Tietoa meistä</a></li>
+          <li><a href="#yhteys">Yhteystiedot</a></li>
+          <li><a href="#aukiolo">Aukioloajat</a></li>
+        </ul>
+      </div>
     </nav>
   );
 };
