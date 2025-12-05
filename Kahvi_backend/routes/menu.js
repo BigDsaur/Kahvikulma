b855e05771b9ba728c92492ea8424d972108616b
BigDsaur <Joona879@gmail.com>
2025-12-05 14:12:04 +0200

    Menu toimii Postmanilla nyt seuraavaks laitan frontendii yhteyden

---
+++ Kahvi_backend/routes/menu.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const MenuItem = require('../models/MenuItem');
+
+// GET all menu items
+router.get('/', async (req, res) => {
+  try {
+    const items = await MenuItem.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// POST new menu item
+router.post('/', async (req, res) => {
+  const { nimi, hinta, ainesosat, luokka, kuvaus, erikoisruokavalio } = req.body;
+
+  try {
+    const item = new MenuItem({
+      nimi,
+      hinta,
+      ainesosat,
+      luokka,
+      kuvaus,
+      erikoisruokavalio
+    });
+
+    const saved = await item.save();
+    res.status(201).json(saved);
+
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// PUT update menu item
+router.put('/:id', async (req, res) => {
+  try {
+    const updated = await MenuItem.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+
+    res.json(updated);
+
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE menu item
+router.delete('/:id', async (req, res) => {
+  try {
+    await MenuItem.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Menu item deleted' });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
