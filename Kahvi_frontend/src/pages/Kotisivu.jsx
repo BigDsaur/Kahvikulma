3184e7905ebb8488890ac6c9e5494bc7024806d7
BigDsaur <Joona879@gmail.com>
2025-12-13 16:04:01 +0200

    css parannusta

--- Kahvi_frontend/src/pages/Kotisivu.jsx
+++ Kahvi_frontend/src/pages/Kotisivu.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./Kotisivu.css";
 import Navbar from "../components/Navbarkoti";
+import Kukkasohva from "../assets/Kukkasohva.jpg";
 
 const Kotisivu = () => {
   const [hours, setHours] = useState([]);
@@ -44,18 +45,24 @@ const Kotisivu = () => {
     <>
       <Navbar />
 
-      {/* —————— Tietoa meistä —————— */}
-      <section id="meista" className="section about">
-        <div className="content">
-          <h2>Tietoa meistä</h2>
-          <p>Kahvikulma on kolmen siskoksen pitämä kahvila Mäntyharjun keskustassa. Kahvila avasi ovensa syyskuussa 2025.</p>
-          <p>Kahvilastamme löytyy päivittäin vaihtuva valikoima erilaisia makeita ja suolaisia herkkuja. Leivomme ja paistamme päivittäin muun muassa tuoretta pullaa, viinereitä ja munkkeja.</p>
-          <p>Lämpimästi tervetuloa Kahvikulmaan!</p>
-          <p>Terkuin </p>
-          <p>Krakaun siskokset!</p>
+      <section id="meista" className="section meista">
+        <div className="meista-container">
+
+          <div className="meista-image">
+            <img src={Kukkasohva} alt="Kahvilan tunnelma" />
+          </div>
+
+          <div className="meista-text">
+            <h2>Tietoa meistä</h2>
+
+            <p>Kahvikulma on kolmen siskoksen pitämä kahvila Mäntyharjun keskustassa. Kahvila avasi ovensa syyskuussa 2025.</p>
+            <p>Kahvilastamme löytyy päivittäin vaihtuva valikoima erilaisia makeita ja suolaisia herkkuja. Leivomme ja paistamme päivittäin muun muassa tuoretta pullaa, viinereitä ja munkkeja.</p>
+            <p>Lämpimästi tervetuloa Kahvikulmaan!</p>
+            <p>Terveisin Krakaun siskokset!</p>
+          </div>
+
         </div>
       </section>
-
       <section id="aukiolo" className="section opening-hours">
         <h2>Aukioloajat</h2>
         <ul style={{ listStyle: "none", padding: 0 }}>
