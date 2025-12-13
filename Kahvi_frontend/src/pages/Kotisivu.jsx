ad5eb3934b301fc16cb9fea10ae2fae821b343a3
Severi <severi0506@gmail.com>
2025-12-13 15:57:25 +0200

    Aukiolo ja yhteystiedot siirretty ja css

--- Kahvi_frontend/src/pages/Kotisivu.jsx
+++ Kahvi_frontend/src/pages/Kotisivu.jsx
@@ -21,44 +21,20 @@ const Kotisivu = () => {
       <section id="meista" className="section about">
         <div className="content">
           <h2>Tietoa meistä</h2>
-          <p>Kahvikulma on kolmen siskoksen pitämä kahvila Mäntyharjun keskustassa. Kahvila avasi ovensa syyskuussa 2025.</p>
-          <p>Kahvilastamme löytyy päivittäin vaihtuva valikoima makeita ja suolaisia herkkuja. Leivomme ja paistamme päivittäin muun muassa tuoretta pullaa, viinereitä ja munkkeja.</p>
+          <p>
+            Kahvikulma on kolmen siskoksen pitämä kahvila Mäntyharjun
+            keskustassa. Kahvila avasi ovensa syyskuussa 2025.
+          </p>
+          <p>
+            Kahvilastamme löytyy päivittäin vaihtuva valikoima erilaisia makeita
+            ja suolaisia herkkuja. Leivomme ja paistamme päivittäin muun
+            muassa tuoretta pullaa, viinereitä ja munkkeja.
+          </p>
           <p>Lämpimästi tervetuloa Kahvikulmaan!</p>
           <p>Terkuin</p>
           <p>Krakaun siskokset!</p>
         </div>
       </section>
-
-      {/* —————— Aukioloajat —————— */}
-      <section id="aukiolo" className="section opening-hours">
-        <h2>Aukioloajat</h2>
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          {hours.length > 0 ? (
-            hours.map((hour) => (
-              <li key={hour._id}>
-                {hour.day}{" "}
-                {hour.open === "Suljettu"
-                  ? "Suljettu"
-                  : `${hour.open} - ${hour.close}`}
-              </li>
-            ))
-          ) : (
-            <p>Ladataan aukioloaikoja...</p>
-          )}
-        </ul>
-      </section>
-
-      {/* —————— Yhteystiedot —————— */}
-      <section id="yhteys" className="section contact">
-        <div className="content">
-          <h2>Yhteystiedot</h2>
-          <p>Pentinpolku 1, 52700 Mäntyharju (Mäntyharjun liikekeskus)</p>
-          <p>puh. 0458529318</p>
-          <p>info@kahvila.fi</p>
-          <p>Vastaamme sähköposteihin arkisin mahdollisimman nopeasti.</p>
-        </div>
-      </section>
-
     </>
   );
 };
