70930598be7e39506b58deae3f4e4ab079a6ba14
Severi <severi0506@gmail.com>
2025-12-13 15:29:40 +0200

    Testi pois

--- Kahvi_backend/testmongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connection successful!");
-    mongoose.disconnect(); // disconnect after testing
-  })
-  .catch(err => console.error("MongoDB connection error:", err));
